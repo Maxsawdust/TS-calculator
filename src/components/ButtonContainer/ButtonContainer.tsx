195b78b61f9262859c42e867a030c3d455c60cb8
Max Saunders <maxsaunders@Maxs-MacBook-Pro.local>
2025-03-27 15:11:28 +0000

    create main styling

--- src/components/ButtonContainer/ButtonContainer.tsx
+++ src/components/ButtonContainer/ButtonContainer.tsx
@@ -1,47 +1,48 @@
 import { CalculatorButton } from "../";
 import "./ButtonContainer.css";
 
+// defining the type of content/props that buttons will have
+export interface ButtonContentTypes {
+  content: string;
+  color?: "dark" | "orange";
+  width?: number;
+}
+
 export default function ButtonContainer() {
-  const bckSpc = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height="24px"
-      viewBox="0 -960 960 960"
-      width="24px"
-      fill="#000">
-      <path d="m456-320 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 160q-19 0-36-8.5T296-192L80-480l216-288q11-15 28-23.5t36-8.5h440q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H360ZM180-480l180 240h440v-480H360L180-480Zm400 0Z" />
-    </svg>
-  );
-  const buttonContents: (string | React.ReactElement)[] = [
-    "%",
-    "CE",
-    "C",
-    bckSpc,
-    "1/𝑥",
-    "x²",
-    "√a",
-    "÷",
-    "7",
-    "8",
-    "9",
-    "×",
-    "4",
-    "5",
-    "6",
-    "-",
-    "1",
-    "2",
-    "3",
-    "+",
-    "±",
-    "0",
-    ".",
-    "=",
+  // array to store button information
+  const buttonContents: ButtonContentTypes[] = [
+    { content: "C", color: "dark" },
+    { content: "±", color: "dark" },
+    { content: "%", color: "dark" },
+    { content: "÷", color: "orange" },
+    { content: "7" },
+    { content: "8" },
+    { content: "9" },
+    { content: "×", color: "orange" },
+    { content: "4" },
+    { content: "5" },
+    { content: "6" },
+    { content: "-", color: "orange" },
+    { content: "1" },
+    { content: "2" },
+    { content: "3" },
+    { content: "+", color: "orange" },
+    { content: "0", width: 2 },
+    { content: "." },
+    { content: "=", color: "orange" },
   ];
+
   return (
     <div className="ButtonContainer">
+      {/* maping through array to display buttons */}
       {buttonContents.map((symbol) => {
-        return <CalculatorButton content={symbol} />;
+        return (
+          <CalculatorButton
+            content={symbol.content}
+            color={symbol.color && symbol.color}
+            width={symbol.width && symbol.width}
+          />
+        );
       })}
     </div>
   );
--- src/components/CalculatorButton/CalculatorButton.tsx
+++ src/components/CalculatorButton/CalculatorButton.tsx
@@ -1,12 +1,28 @@
 import "./CalculatorButton.css";
+import { ButtonContentTypes } from "../ButtonContainer/ButtonContainer";
 
-interface CalculatorButtonProps {
-  content: string | React.ReactElement;
-}
+export default function CalculatorButton({
+  content,
+  color,
+  width,
+}: ButtonContentTypes) {
+  // map to store colours based on props
+  const buttonColors = {
+    dark: "#676767",
+    orange: "orange",
+  };
 
-export default function CalculatorButton({ content }: CalculatorButtonProps) {
   return (
     //
-    <div className="CalculatorButton">{content}</div>
+    <div
+      className="CalculatorButton"
+      // defining backgroundColor and gridColum properties based on props
+      style={{
+        backgroundColor: color ? buttonColors[color] : "#9a9a9a",
+        gridColumn: width && `span ${width}`,
+      }}
+    >
+      {content}
+    </div>
   );
 }
